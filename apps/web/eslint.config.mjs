beca3b810a16c4efad2e2f48a28d618f88722d6c
mkizka <g.mkizka@gmail.com>
2024-10-28 00:16:52 +0900

    update

--- apps/web/eslint.config.mjs
+++ apps/web/eslint.config.mjs
@@ -1,10 +1,11 @@
 import { configs } from "@mkizka/eslint-config";
+import next from "eslint-config-next-flat";
 
 export default [
   {
     ignores: ["**/.next"],
   },
+  next,
   ...configs.typescript(),
-  ...configs.react(),
   ...configs.tailwind(),
 ];
