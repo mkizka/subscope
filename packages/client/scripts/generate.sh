7c0430b341260a3f7509cb8c8c76ecd4e43fb24f
mkizka <g.mkizka@gmail.com>
2024-12-29 23:50:28 +0900

    update

--- packages/client/scripts/generate.sh
+++ packages/client/scripts/generate.sh
@@ -4,8 +4,10 @@ set -euo pipefail
 ATPROTO_DIR=../../atproto/lexicons
 LEXICONS_DIR=../../lexicons
 
-cp -r $ATPROTO_DIR/* $LEXICONS_DIR
+cp -r $ATPROTO_DIR/app $LEXICONS_DIR
+cp -r $ATPROTO_DIR/com $LEXICONS_DIR
 
 LEXICONS=$(find $LEXICONS_DIR -name '*.json' -type f)
+echo 'Starting client code generation...'
 echo y | pnpm lex gen-api ./src/generated/api $LEXICONS
 echo y | pnpm lex gen-server ./src/generated/server $LEXICONS
