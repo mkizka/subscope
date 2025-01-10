c44c152c7467e5cee9edfb0f2b24e3df215e2f76
mkizka <g.mkizka@gmail.com>
2025-01-10 23:50:11 +0900

    db logger

--- apps/appview/src/infrastructure/db.ts
+++ apps/appview/src/infrastructure/db.ts
@@ -1,5 +1,11 @@
 import { createDatabase } from "@dawn/db";
 
 import { env } from "../shared/env.js";
+import { createLogger } from "../shared/logger.js";
 
-export const db = createDatabase(env.DATABASE_URL);
+const logger = createLogger("db");
+
+export const db = createDatabase({
+  url: env.DATABASE_URL,
+  logger,
+});
