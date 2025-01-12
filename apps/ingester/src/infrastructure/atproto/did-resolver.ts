e0cc7326a1ad48c4233f3477db34d5287f9894b6
mkizka <g.mkizka@gmail.com>
2025-01-12 17:47:15 +0900

    add did resolver logging

--- apps/ingester/src/infrastructure/atproto/did-resolver.ts
+++ apps/ingester/src/infrastructure/atproto/did-resolver.ts
@@ -1,10 +1,34 @@
-import { DidResolver as Resolver, MemoryCache } from "@atproto/identity";
+import { DidResolver as BaseDidResolver, MemoryCache } from "@atproto/identity";
 
 import type { IDidResolver } from "../../application/interfaces/did-resolver.js";
 import { env } from "../../shared/env.js";
 import { createLogger } from "../../shared/logger.js";
 
 const logger = createLogger("DidResolver");
+
+let resolvedCount = 0;
+let cacheExistsCount = 0;
+
+class Resolver extends BaseDidResolver {
+  async resolve(did: string) {
+    resolvedCount++;
+    if (this.cache) {
+      const fromCache = await this.cache.checkCache(did);
+      if (fromCache && !fromCache.expired) {
+        cacheExistsCount++;
+      }
+    }
+    logger.info(
+      {
+        resolvedCount,
+        cacheExistsCount,
+        ratio: cacheExistsCount / resolvedCount,
+      },
+      `Resolving DID: ${did}`,
+    );
+    return super.resolve(did);
+  }
+}
 
 export class DidResolver implements IDidResolver {
   readonly resolver: Resolver;
