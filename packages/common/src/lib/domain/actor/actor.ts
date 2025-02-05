02da94181780f0d6c4f058d2b74f2ce193fac7d8
mkizka <g.mkizka@gmail.com>
2025-02-06 01:37:26 +0900

    refactor actor constructor to use ActorParams type

--- packages/common/src/lib/domain/actor/actor.ts
+++ packages/common/src/lib/domain/actor/actor.ts
@@ -3,11 +3,16 @@ import { asDid, type Did } from "@atproto/did";
 import type { Handle } from "../../utils/handle.js";
 import { asHandle } from "../../utils/handle.js";
 
+type ActorParams = {
+  did: string;
+  handle?: string;
+};
+
 export class Actor {
   readonly did: Did;
   readonly handle: Handle | null; // handle can fail to resolve
 
-  constructor(params: { did: string; handle?: string | null }) {
+  constructor(params: ActorParams) {
     this.did = asDid(params.did);
     this.handle = params.handle ? asHandle(params.handle) : null;
   }
