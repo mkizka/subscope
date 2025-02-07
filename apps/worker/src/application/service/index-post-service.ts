94216b1110d3a90670ae4b46c6af84ba988ed869
mkizka <g.mkizka@gmail.com>
2025-02-07 12:34:18 +0900

    RecordとPostを変換可能に

--- apps/worker/src/application/service/index-post-service.ts
+++ apps/worker/src/application/service/index-post-service.ts
@@ -1,6 +1,3 @@
-import { jsonToLex } from "@atproto/lexicon";
-import type { AppBskyFeedPost } from "@dawn/client";
-import client from "@dawn/client";
 import type { Record, TransactionContext } from "@dawn/common/domain";
 import { Post } from "@dawn/common/domain";
 
@@ -10,20 +7,8 @@ export class IndexPostService {
   constructor(private readonly postRepository: IPostRepository) {}
   static inject = ["postRepository"] as const;
 
-  private assertRecord(json: unknown): asserts json is AppBskyFeedPost.Record {
-    client.lexicons.assertValidRecord("app.bsky.feed.post", jsonToLex(json));
-  }
-
   async upsert({ ctx, record }: { ctx: TransactionContext; record: Record }) {
-    this.assertRecord(record.json);
-    const post = new Post({
-      uri: record.uri,
-      cid: record.cid,
-      actorDid: record.actorDid,
-      text: record.json.text,
-      langs: record.json.langs ?? [],
-      createdAt: new Date(record.json.createdAt),
-    });
+    const post = Post.from(record);
     await this.postRepository.upsert({ ctx, post });
   }
 }
--- packages/common/src/lib/domain/post/post.ts
+++ packages/common/src/lib/domain/post/post.ts
@@ -1,6 +1,10 @@
 import { asDid, type Did } from "@atproto/did";
+import { jsonToLex } from "@atproto/lexicon";
 import { AtUri } from "@atproto/syntax";
+import type { AppBskyFeedPost } from "@dawn/client";
+import client from "@dawn/client";
 
+import type { Record } from "../record.js";
 type PostParams = {
   uri: AtUri | string;
   cid: string;
@@ -9,6 +13,10 @@ type PostParams = {
   langs: string[] | null;
   createdAt: Date;
 };
+
+function assertRecord(json: unknown): asserts json is AppBskyFeedPost.Record {
+  client.lexicons.assertValidRecord("app.bsky.feed.post", jsonToLex(json));
+}
 
 export class Post {
   readonly uri: AtUri;
@@ -25,5 +33,17 @@ export class Post {
     this.text = params.text;
     this.langs = params.langs;
     this.createdAt = params.createdAt;
+  }
+
+  static from(record: Record) {
+    assertRecord(record.json);
+    return new Post({
+      uri: record.uri,
+      cid: record.cid,
+      actorDid: record.actorDid,
+      text: record.json.text,
+      langs: record.json.langs ?? [],
+      createdAt: new Date(record.json.createdAt),
+    });
   }
 }
