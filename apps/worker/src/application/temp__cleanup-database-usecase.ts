306553c70b39448e4d7773beabfb523aeb6e37c4
mkizka <g.mkizka@gmail.com>
2025-02-11 18:24:27 +0900

    FOREIGN_KEY_CHECKS無効化

--- apps/worker/src/application/temp__cleanup-database-usecase.ts
+++ apps/worker/src/application/temp__cleanup-database-usecase.ts
@@ -6,11 +6,13 @@ export class Temp__CleanupDatabaseUseCase {
 
   async execute() {
     await this.transactionManager.transaction(async (ctx) => {
+      await ctx.db.execute("SET FOREIGN_KEY_CHECKS = 0;");
       await ctx.db.execute("TRUNCATE TABLE blobs;");
       await ctx.db.execute("TRUNCATE TABLE posts;");
       await ctx.db.execute("TRUNCATE TABLE profiles;");
       await ctx.db.execute("TRUNCATE TABLE records;");
       await ctx.db.execute("TRUNCATE TABLE actors;");
+      await ctx.db.execute("SET FOREIGN_KEY_CHECKS = 1;");
     });
   }
 }
