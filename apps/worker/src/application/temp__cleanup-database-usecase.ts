a230597a0c6db586417685cb283aa93593ac1b0a
mkizka <g.mkizka@gmail.com>
2025-02-16 14:23:23 +0900

    削除処理改善

--- apps/worker/src/application/temp__cleanup-database-usecase.ts
+++ apps/worker/src/application/temp__cleanup-database-usecase.ts
@@ -1,27 +1,31 @@
 import type { DatabaseClient } from "@dawn/common/domain";
 import { schema } from "@dawn/db";
-import { inArray } from "drizzle-orm";
-
-const chunk = <T>(array: T[], n: number): T[][] => {
-  const newArrayLength = Math.ceil(array.length / n);
-  return Array.from({ length: newArrayLength }, (_, i) =>
-    array.slice(i * n, i * n + n),
-  );
-};
+import { count, inArray, lt } from "drizzle-orm";
 
 export class Temp__CleanupDatabaseUseCase {
   constructor(private readonly db: DatabaseClient) {}
   static inject = ["db"] as const;
 
   async execute(jobLogger: { log: (message: string) => void }) {
-    const allPosts = await this.db
-      .select({ uri: schema.posts.uri })
-      .from(schema.posts);
-    jobLogger.log(`Found ${allPosts.length} posts`);
-    const splittedPosts = chunk(allPosts, 100);
-    for (const [i, postChunk] of splittedPosts.entries()) {
-      jobLogger.log(`Deleting posts (${i + 1}/${splittedPosts.length})`);
-      const uris = postChunk.map((post) => post.uri);
+    jobLogger.log("Starting cleanup database");
+    const ONE_HOUR_AGO = new Date(Date.now() - 1000 * 60 * 60);
+    const [postCountsToDelete] = await this.db
+      .select({
+        count: count(),
+      })
+      .from(schema.posts)
+      .where(lt(schema.posts.createdAt, ONE_HOUR_AGO));
+
+    jobLogger.log(`Found ${postCountsToDelete!.count} posts to delete`);
+    const range = Math.ceil(postCountsToDelete!.count / 100);
+    for (const i of Array(range).keys()) {
+      jobLogger.log(`Deleting ${i + 100} posts`);
+      const posts = await this.db
+        .select({ uri: schema.posts.uri })
+        .from(schema.posts)
+        .where(lt(schema.posts.createdAt, ONE_HOUR_AGO))
+        .limit(100);
+      const uris = posts.map((post) => post.uri);
       await this.db
         .delete(schema.records)
         .where(inArray(schema.posts.uri, uris));
