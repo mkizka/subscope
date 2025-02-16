ee28a0af334fc94de19905b2ce78bfad007881ba
mkizka <g.mkizka@gmail.com>
2025-02-16 13:41:30 +0900

    投稿削除を100個ずつに分割

--- apps/worker/src/application/temp__cleanup-database-usecase.ts
+++ apps/worker/src/application/temp__cleanup-database-usecase.ts
@@ -1,20 +1,30 @@
-import type { ITransactionManager } from "@dawn/common/domain";
+import type { DatabaseClient } from "@dawn/common/domain";
 import { schema } from "@dawn/db";
 import { inArray } from "drizzle-orm";
 
-export class Temp__CleanupDatabaseUseCase {
-  constructor(private readonly transactionManager: ITransactionManager) {}
-  static inject = ["transactionManager"] as const;
+const chunk = <T>(array: T[], n: number): T[][] => {
+  const newArrayLength = Math.ceil(array.length / n);
+  return Array.from({ length: newArrayLength }, (_, i) =>
+    array.slice(i * n, i * n + n),
+  );
+};
 
-  async execute() {
-    await this.transactionManager.transaction(async (ctx) => {
-      const posts = await ctx.db
-        .select({ uri: schema.posts.uri })
-        .from(schema.posts);
-      const uris = posts.map((post) => post.uri);
-      await ctx.db
+export class Temp__CleanupDatabaseUseCase {
+  constructor(private readonly db: DatabaseClient) {}
+  static inject = ["db"] as const;
+
+  async execute(jobLogger: { log: (message: string) => void }) {
+    const allPosts = await this.db
+      .select({ uri: schema.posts.uri })
+      .from(schema.posts);
+    jobLogger.log(`Found ${allPosts.length} posts`);
+    const splittedPosts = chunk(allPosts, 100);
+    for (const [i, postChunk] of splittedPosts.entries()) {
+      jobLogger.log(`Deleting posts (${i + 1}/${splittedPosts.length})`);
+      const uris = postChunk.map((post) => post.uri);
+      await this.db
         .delete(schema.records)
         .where(inArray(schema.posts.uri, uris));
-    });
+    }
   }
 }
