a7b1a5bea07ce923f8ab90e56801a979c7cebd45
mkizka <g.mkizka@gmail.com>
2025-03-09 21:32:09 +0900

    lexiconsディレクトリに直接入れる

--- packages/client/scripts/generate.sh
+++ packages/client/scripts/generate.sh
@@ -1,13 +1,7 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-ATPROTO_DIR=./node_modules/.cache/lexicons
-LEXICONS_DIR=../../lexicons
-
-cp -r $ATPROTO_DIR/app $LEXICONS_DIR
-cp -r $ATPROTO_DIR/com $LEXICONS_DIR
-
-LEXICONS=$(find $LEXICONS_DIR -name '*.json' -type f)
+LEXICONS=$(find ../../lexicons -name '*.json' -type f)
 echo 'Starting client code generation...'
 yes | pnpm lex gen-api ./src/generated/api $LEXICONS &
 yes | pnpm lex gen-server ./src/generated/server $LEXICONS &
