dd0a80b8555321cc216a7ed0855a6216677cbf0d
mkizka <g.mkizka@gmail.com>
2025-03-11 23:40:06 +0900

    ユースケースからIndexCommitServiceを分離

--- apps/worker/src/application/index-commit-use-case.ts
+++ apps/worker/src/application/index-commit-use-case.ts
@@ -1,72 +1,32 @@
 import { type ITransactionManager, Record } from "@dawn/common/domain";
-import type { SupportedCollection } from "@dawn/common/utils";
 
 import type { IndexCommitCommand } from "./index-commit-command.js";
-import type { IIndexColectionService } from "./interfaces/index-collection-service.js";
-import type { IRecordRepository } from "./interfaces/record-repository.js";
-import type { IndexActorService } from "./service/index-actor-service.js";
-import type { IndexPostService } from "./service/index-post-service.js";
-import type { IndexProfileService } from "./service/index-profile-service.js";
-
-type IndexCollectionServiceMap = {
-  [key in SupportedCollection]: IIndexColectionService;
-};
-
-const isValidRecord = (record: Record) => {
-  // Postgresには`\u0000`を含む文字列を保存できないため
-  return !JSON.stringify(record.getJson()).includes("\\u0000");
-};
+import type { IndexCommitService } from "./service/index-commit-service.js";
 
 export class IndexCommitUseCase {
-  private readonly services: IndexCollectionServiceMap;
-
   constructor(
     private readonly transactionManager: ITransactionManager,
-    private readonly recordRepository: IRecordRepository,
-    private readonly indexActorService: IndexActorService,
-    indexPostService: IndexPostService,
-    indexProfileService: IndexProfileService,
-  ) {
-    this.services = {
-      "app.bsky.feed.post": indexPostService,
-      "app.bsky.actor.profile": indexProfileService,
-    };
-  }
-  static inject = [
-    "transactionManager",
-    "recordRepository",
-    "indexActorService",
-    "indexPostService",
-    "indexProfileService",
-  ] as const;
+    private readonly indexCommitService: IndexCommitService,
+  ) {}
+  static inject = ["transactionManager", "indexCommitService"] as const;
 
-  async execute(command: IndexCommitCommand) {
-    await command.jobLogger.log(command.commit.uri.toString());
+  async execute({ commit, jobLogger }: IndexCommitCommand) {
+    await jobLogger.log(commit.uri.toString());
     await this.transactionManager.transaction(async (ctx) => {
-      await this.indexActorService.createIfNotExists({
-        ctx,
-        did: command.commit.did,
-      });
-      const indexService = this.services[command.commit.collection];
-      switch (command.commit.operation) {
+      switch (commit.operation) {
         case "create":
         case "update": {
           const record = Record.fromJson({
-            uri: command.commit.uri,
-            cid: command.commit.cid,
-            json: command.commit.record,
+            uri: commit.uri,
+            cid: commit.cid,
+            json: commit.record,
           });
-          if (!isValidRecord(record)) {
-            await command.jobLogger.log("Invalid record: null character found");
-            break;
-          }
-          await this.recordRepository.upsert({ ctx, record });
-          await indexService.upsert({ ctx, record });
+          await this.indexCommitService.upsert({ ctx, record, jobLogger });
           break;
         }
         case "delete": {
           // Related data is also deleted by cascade
-          await this.recordRepository.delete({ ctx, uri: command.commit.uri });
+          await this.indexCommitService.delete({ ctx, uri: commit.uri });
           break;
         }
       }
---
+++ apps/worker/src/application/service/index-commit-service.ts
@@ -0,0 +1,68 @@
+import type { AtUri } from "@atproto/syntax";
+import type { TransactionContext } from "@dawn/common/domain";
+import type { Record } from "@dawn/common/domain";
+import { type SupportedCollection } from "@dawn/common/utils";
+
+import type { JobLogger } from "../../shared/job.js";
+import type { IIndexColectionService } from "../interfaces/index-collection-service.js";
+import type { IRecordRepository } from "../interfaces/record-repository.js";
+import type { IndexActorService } from "./index-actor-service.js";
+import type { IndexPostService } from "./index-post-service.js";
+import type { IndexProfileService } from "./index-profile-service.js";
+
+type IndexCollectionServiceMap = {
+  [key in SupportedCollection]: IIndexColectionService;
+};
+
+const isValidRecord = (record: Record) => {
+  // Postgresには`\u0000`を含む文字列を保存できないため
+  return !JSON.stringify(record.getJson()).includes("\\u0000");
+};
+
+export class IndexCommitService {
+  private readonly services: IndexCollectionServiceMap;
+
+  constructor(
+    private readonly recordRepository: IRecordRepository,
+    private readonly indexActorService: IndexActorService,
+    indexPostService: IndexPostService,
+    indexProfileService: IndexProfileService,
+  ) {
+    this.services = {
+      "app.bsky.feed.post": indexPostService,
+      "app.bsky.actor.profile": indexProfileService,
+    };
+  }
+  static inject = [
+    "transactionManager",
+    "recordRepository",
+    "indexActorService",
+    "indexPostService",
+    "indexProfileService",
+  ] as const;
+
+  async upsert({
+    ctx,
+    record,
+    jobLogger,
+  }: {
+    ctx: TransactionContext;
+    record: Record;
+    jobLogger: JobLogger;
+  }) {
+    await this.indexActorService.createIfNotExists({
+      ctx,
+      did: record.actorDid,
+    });
+    if (!isValidRecord(record)) {
+      await jobLogger.log("Invalid record: null character found");
+      return;
+    }
+    await this.recordRepository.upsert({ ctx, record });
+    await this.services[record.getCollection()].upsert({ ctx, record });
+  }
+
+  async delete({ ctx, uri }: { ctx: TransactionContext; uri: AtUri }) {
+    await this.recordRepository.delete({ ctx, uri });
+  }
+}
