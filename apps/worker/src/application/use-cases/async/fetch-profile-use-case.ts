0c80a4c718030921edaf9a4007df53be27b3d21a
mkizka <g.mkizka@gmail.com>
2025-06-27 01:05:52 +0900

    fetchProfile処理をindexCommitServiceに置き換え

--- apps/worker/src/application/use-cases/async/fetch-profile-use-case.ts
+++ apps/worker/src/application/use-cases/async/fetch-profile-use-case.ts
@@ -1,27 +1,20 @@
 import type { Did } from "@atproto/did";
 import type { ITransactionManager } from "@repo/common/domain";
-import { Profile } from "@repo/common/domain";
 
 import type { JobLogger } from "../../../shared/job.js";
 import type { IProfileRecordFetcher } from "../../interfaces/external/profile-record-fetcher.js";
-import type { IActorRepository } from "../../interfaces/repositories/actor-repository.js";
-import type { IProfileRepository } from "../../interfaces/repositories/profile-repository.js";
-import type { IRecordRepository } from "../../interfaces/repositories/record-repository.js";
+import type { IndexCommitService } from "../../services/index-commit-service.js";
 
 export class FetchProfileUseCase {
   constructor(
     private readonly profileRecordFetcher: IProfileRecordFetcher,
-    private readonly actorRepository: IActorRepository,
-    private readonly recordRepository: IRecordRepository,
-    private readonly profileRepository: IProfileRepository,
     private readonly transactionManager: ITransactionManager,
+    private readonly indexCommitService: IndexCommitService,
   ) {}
   static inject = [
     "profileRecordFetcher",
-    "actorRepository",
-    "recordRepository",
-    "profileRepository",
     "transactionManager",
+    "indexCommitService",
   ] as const;
 
   async execute({
@@ -37,13 +30,10 @@ export class FetchProfileUseCase {
       return;
     }
     await this.transactionManager.transaction(async (ctx) => {
-      // indexCommitServiceから呼ばれるジョブなのでActorが存在する前提だが、
-      // actorの作成完了前にこのジョブが実行された場合actorが必要なので作成する
-      await this.actorRepository.createIfNotExists({ ctx, did });
-      await this.recordRepository.upsert({ ctx, record });
-      await this.profileRepository.upsert({
+      await this.indexCommitService.upsert({
         ctx,
-        profile: Profile.from(record),
+        record,
+        jobLogger,
       });
     });
   }
