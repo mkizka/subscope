29c6be179262e9bffd944d7d3b07b63515241289
mkizka <g.mkizka@gmail.com>
2025-06-28 02:27:17 +0900

    coverage追加

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,5 +3,14 @@ import { defineConfig } from "vitest/config";
 export default defineConfig({
   test: {
     projects: ["apps/*", "packages/*"],
+    coverage: {
+      exclude: [
+        "**/generated/**",
+        "**/dist/**",
+        "**/eslint.config.?(m)js",
+        "**/vitest.config.ts",
+        "**/build.mjs",
+      ],
+    },
   },
 });
