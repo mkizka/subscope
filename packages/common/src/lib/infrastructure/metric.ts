8af96b9c51eb714bf0644d078af9a3b8ede6f4ee
mkizka <g.mkizka@gmail.com>
2025-06-29 23:09:23 +0900

    メトリクスの初期化をコンストラクタに移動

--- packages/common/src/lib/infrastructure/metric.ts
+++ packages/common/src/lib/infrastructure/metric.ts
@@ -8,62 +8,6 @@ import type {
   LabelsValue,
 } from "../domain/interfaces/metric.js";
 
-const counters = {
-  did_cache_hit_total: new client.Counter({
-    name: "did_cache_hit_total",
-    help: "Total number of did resolver cache hits",
-  }),
-  did_cache_miss_total: new client.Counter({
-    name: "did_cache_miss_total",
-    help: "Total number of did resolver cache misses",
-  }),
-  did_resolve_total: new client.Counter({
-    name: "did_resolve_total",
-    help: "Total number of did resolves",
-  }),
-  did_resolve_error_total: new client.Counter({
-    name: "did_resolve_error_total",
-    help: "Total number of did resolve errors",
-  }),
-  ingester_events_account_total: new client.Counter({
-    name: "ingester_events_account_total",
-    help: "Total number of account events processed by the ingester",
-  }),
-  ingester_events_identity_total: new client.Counter({
-    name: "ingester_events_identity_total",
-    labelNames: ["change_handle"],
-    help: "Total number of identity events processed by the ingester",
-  }),
-  ingester_events_commit_total: new client.Counter({
-    name: "ingester_events_commit_total",
-    labelNames: ["collection"],
-    help: "Total number of commit events processed by the ingester",
-  }),
-  blob_proxy_cache_hit_total: new client.Counter({
-    name: "blob_proxy_cache_hit_total",
-    help: "Total number of blob cache hits",
-  }),
-  blob_proxy_cache_miss_total: new client.Counter({
-    name: "blob_proxy_cache_miss_total",
-    help: "Total number of blob cache misses",
-  }),
-} satisfies {
-  [key in CounterKey]: client.Counter;
-};
-
-const gauges = {
-  ingester_events_time_delay: new client.Gauge({
-    name: "ingester_events_time_delay",
-    help: "Time delay of events processed by the ingester",
-  }),
-  ingester_websocket_connection_state: new client.Gauge({
-    name: "ingester_websocket_connection_state",
-    help: "State of the websocket connection",
-  }),
-} satisfies {
-  [key in GaugeKey]: client.Gauge;
-};
-
 const connectionStates = {
   open: 1,
   error: 2,
@@ -73,21 +17,90 @@ const connectionStates = {
   [key in ConnectionStates]: number;
 };
 
+type CounterMap = {
+  [key in CounterKey]: client.Counter;
+};
+
+type GaugeMap = {
+  [key in GaugeKey]: client.Gauge;
+};
+
 export class MetricReporter implements IMetricReporter {
+  private readonly counters: CounterMap;
+  private readonly gauges: GaugeMap;
+
+  constructor() {
+    this.counters = {
+      did_cache_hit_total: new client.Counter({
+        name: "did_cache_hit_total",
+        help: "Total number of did resolver cache hits",
+      }),
+      did_cache_miss_total: new client.Counter({
+        name: "did_cache_miss_total",
+        help: "Total number of did resolver cache misses",
+      }),
+      did_resolve_total: new client.Counter({
+        name: "did_resolve_total",
+        help: "Total number of did resolves",
+      }),
+      did_resolve_error_total: new client.Counter({
+        name: "did_resolve_error_total",
+        help: "Total number of did resolve errors",
+      }),
+      ingester_events_account_total: new client.Counter({
+        name: "ingester_events_account_total",
+        help: "Total number of account events processed by the ingester",
+      }),
+      ingester_events_identity_total: new client.Counter({
+        name: "ingester_events_identity_total",
+        labelNames: ["change_handle"],
+        help: "Total number of identity events processed by the ingester",
+      }),
+      ingester_events_commit_total: new client.Counter({
+        name: "ingester_events_commit_total",
+        labelNames: ["collection"],
+        help: "Total number of commit events processed by the ingester",
+      }),
+      blob_proxy_cache_hit_total: new client.Counter({
+        name: "blob_proxy_cache_hit_total",
+        help: "Total number of blob cache hits",
+      }),
+      blob_proxy_cache_miss_total: new client.Counter({
+        name: "blob_proxy_cache_miss_total",
+        help: "Total number of blob cache misses",
+      }),
+    };
+    this.gauges = {
+      ingester_events_time_delay: new client.Gauge({
+        name: "ingester_events_time_delay",
+        help: "Time delay of events processed by the ingester",
+      }),
+      ingester_websocket_connection_state: new client.Gauge({
+        name: "ingester_websocket_connection_state",
+        help: "State of the websocket connection",
+      }),
+    };
+  }
+
   increment(key: CounterKey, labels?: LabelsValue): void {
     if (labels) {
-      counters[key].inc(labels);
+      this.counters[key].inc(labels);
     } else {
-      counters[key].inc();
+      this.counters[key].inc();
     }
   }
+
   setConnectionStateGauge(state: ConnectionStates) {
-    gauges.ingester_websocket_connection_state.set(connectionStates[state]);
+    this.gauges.ingester_websocket_connection_state.set(
+      connectionStates[state],
+    );
   }
+
   setTimeDelayGauge(timeUs: number) {
-    const dalay = Date.now() - Math.round(timeUs / 1000);
-    gauges.ingester_events_time_delay.set(dalay);
+    const delay = Date.now() - Math.round(timeUs / 1000);
+    this.gauges.ingester_events_time_delay.set(delay);
   }
+
   getMetrics() {
     return client.register.metrics();
   }
