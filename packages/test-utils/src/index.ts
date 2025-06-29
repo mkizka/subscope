2cf69ecbca13bced76b97554f5f4046c93c3b74d
mkizka <g.mkizka@gmail.com>
2025-06-29 23:06:22 +0900

    リネーム

--- packages/test-utils/src/index.ts
+++ packages/test-utils/src/index.ts
@@ -1,5 +1,4 @@
 export * from "./cid.js";
 export * from "./factory.js";
-// export * from "./setup.js";
 export * from "./global-setup.js";
-export * from "./setup-v2.js";
+export * from "./setup.js";
--- packages/test-utils/src/setup.ts
+++ packages/test-utils/src/setup.ts
@@ -1,65 +1,22 @@
-import path from "node:path";
-
-import type { TransactionContext } from "@repo/common/domain";
 import {
   connectionPoolFactory,
   databaseFactory,
   LoggerManager,
 } from "@repo/common/infrastructure";
 import { required } from "@repo/common/utils";
-import type { StartedPostgreSqlContainer } from "@testcontainers/postgresql";
-import { PostgreSqlContainer } from "@testcontainers/postgresql";
-import { execa } from "execa";
 import { createInjector } from "typed-inject";
-import { afterAll, beforeAll } from "vitest";
 
-const createTestInjector = (databaseUrl: string) => {
-  return createInjector()
+export const getTestSetup = () => {
+  const testInjector = createInjector()
     .provideValue("logLevel", "error" as const)
-    .provideValue("databaseUrl", databaseUrl)
+    .provideValue("databaseUrl", required(process.env.TEST_DATABASE_URL))
     .provideClass("loggerManager", LoggerManager)
     .provideFactory("connectionPool", connectionPoolFactory)
     .provideFactory("db", databaseFactory);
-};
-
-interface TestDatabaseSetup {
-  testInjector: ReturnType<typeof createTestInjector>;
-  ctx: TransactionContext;
-}
-
-export function setupTestDatabase() {
-  let testSetup: TestDatabaseSetup;
-  let postgresContainer: StartedPostgreSqlContainer;
-  let connectionPool: ReturnType<typeof connectionPoolFactory>;
-
-  beforeAll(async () => {
-    postgresContainer = await new PostgreSqlContainer(
-      "postgres:16-alpine",
-    ).start();
-    const databaseUrl = postgresContainer.getConnectionUri();
-
-    await execa({
-      cwd: path.resolve(import.meta.dirname, "../../../"),
-      env: {
-        DATABASE_URL: databaseUrl,
-      },
-    })`pnpm db:migrate`;
-
-    const testInjector = createTestInjector(databaseUrl);
-    connectionPool = testInjector.resolve("connectionPool");
-
-    testSetup = {
-      testInjector,
-      ctx: { db: testInjector.resolve("db") },
-    };
-  });
-
-  afterAll(async () => {
-    await connectionPool.end();
-    await postgresContainer.stop();
-  });
-
   return {
-    getSetup: () => required(testSetup),
+    testInjector,
+    ctx: {
+      db: testInjector.resolve("db"),
+    },
   };
-}
+};
