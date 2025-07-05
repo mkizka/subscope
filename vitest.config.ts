81ec870308ef600fe5dd83d499b9b41b69705218
mkizka <g.mkizka@gmail.com>
2025-07-05 18:12:28 +0900

    テストの分離無効化

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from "vitest/config";
 export default defineConfig({
   test: {
     projects: ["apps/*", "packages/*"],
+    isolate: false, // ファイル分離によってコネクションプールが作られ過ぎるのを防ぐため
     coverage: {
       exclude: [
         "**/generated/**",
