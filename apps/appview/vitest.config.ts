b6284edae3175bfaa6a70de0002ab4973c6a2dca
mkizka <g.mkizka@gmail.com>
2025-07-05 18:20:04 +0900

    nameは無くてもいいっぽいので削除

--- apps/appview/vitest.config.ts
+++ apps/appview/vitest.config.ts
@@ -2,7 +2,6 @@ import { defineProject } from "vitest/config";
 
 export default defineProject({
   test: {
-    name: "appview",
     globalSetup: "./vitest.global-setup.ts",
     testTimeout: 120000, // 2 min
     clearMocks: true,
--- apps/worker/vitest.config.ts
+++ apps/worker/vitest.config.ts
@@ -2,7 +2,6 @@ import { defineProject } from "vitest/config";
 
 export default defineProject({
   test: {
-    name: "worker",
     globalSetup: "./vitest.global-setup.ts",
     testTimeout: 120000, // 2 min
     clearMocks: true,
