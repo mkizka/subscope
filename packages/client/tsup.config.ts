bb306c6bd3b2475635bd7438c1693af0165d7351
mkizka <g.mkizka@gmail.com>
2025-07-19 16:08:30 +0900

    tsup削除

--- packages/client/tsup.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from "tsup";
-
-export default defineConfig({
-  entry: ["src/api.ts", "src/server.ts"],
-  format: "esm",
-  sourcemap: true,
-  clean: true,
-  outExtension: () => ({ js: ".mjs" }),
-});
