65ed448c51ec8e4e5924ca8051073ff3c221a3de
mkizka <g.mkizka@gmail.com>
2025-07-20 01:17:53 +0900

    todo追加

--- apps/appview/src/infrastructure/asset-url-builder.ts
+++ apps/appview/src/infrastructure/asset-url-builder.ts
@@ -1,8 +1,10 @@
 import { env } from "../shared/env.js";
 
+// TODO: interfaceかく
 export class AssetUrlBuilder {
   static inject = [] as const;
 
+  // TODO: avatar,bunnerが使われていないので確認
   getAvatarThumbnailUrl(actorDid: string, cid: string): string {
     return `${env.BLOB_PROXY_URL}/images/avatar_thumbnail/${actorDid}/${cid}.jpg`;
   }
