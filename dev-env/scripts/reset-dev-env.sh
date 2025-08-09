4901e93fee6091ee87555bca93c0371e2f058f42
mkizka <g.mkizka@gmail.com>
2025-08-09 12:31:13 +0900

    devコマンド調整

--- dev-env/scripts/reset-dev-env.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-set -euo pipefail
-
-ATPROTO_SHA=$(< ../packages/client/.atproto-sha)
-ATPROTO_DIR="$HOME/.cache/atproto/$ATPROTO_SHA"
-
-echo "[dev-env] Resetting atproto dev server..."
-cd $ATPROTO_DIR/packages/dev-infra && docker compose down -v
--- scripts/dev.sh
+++ scripts/dev.sh
@@ -4,33 +4,31 @@ set -euo pipefail
 cleanup() {
   echo "[dev.sh] Cleaning up..."
   docker compose down
+  echo "[dev.sh] Killing port 2583..."
   pnpm kill-port 2583
 }
 
 main() {
-  # 1. atprotoリポジトリのdockerボリュームを削除して初期値に戻す
-  pnpm --filter @repo/dev-env reset-dev-env
-
-  # 2. atprotoリポジトリをダウンロードしてビルド
+  # 1. atprotoリポジトリをダウンロードしてビルド
   pnpm --filter @repo/dev-env setup-dev-env
 
-  # 3. atprotoリポジトリでmake run-dev-envを実行
+  # 2. atprotoリポジトリでmake run-dev-envを実行
   DEV_ENV_LOG=dev-env/run-dev-env.log
   rm -f $DEV_ENV_LOG
   pnpm --filter @repo/dev-env run-dev-env > $DEV_ENV_LOG 2>&1 &
   pnpm wait-on http://localhost:2583/xrpc/_health
 
-  # 4. このリポジトリのdocker composeを起動
+  # 3. このリポジトリのdocker composeを起動
   echo "[dev.sh] Starting docker containers..."
   docker compose up -d
   # 5432=Postgres, 6379=Redis, 6008=Jetstream
   pnpm wait-on tcp:5432 tcp:6379 tcp:6008
 
-  # 5. dockerコンテナのPostgresにマイグレーション
+  # 4. dockerコンテナのPostgresにマイグレーション
   echo "[dev.sh] Migrating database..."
   pnpm -s db:migrate
 
-  # 6. このリポジトリの開発環境を起動
+  # 5. このリポジトリの開発環境を起動
   echo "[dev.sh] Ready to start development environment."
   pnpm turbo run dev --ui tui
 }
