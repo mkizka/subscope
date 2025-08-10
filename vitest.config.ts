7e958a7d0053b99b326d7609a85caa6f95d001ff
mkizka <g.mkizka@gmail.com>
2025-08-10 19:15:37 +0900

    singleForkで直列実行にする

--- vitest.config.ts
+++ vitest.config.ts
@@ -4,6 +4,12 @@ export default defineConfig({
   test: {
     projects: ["apps/*", "packages/*"],
     isolate: false, // ファイル分離によってコネクションプールが作られ過ぎるのを防ぐため
+    poolOptions: {
+      forks: {
+        // 1パッケージにつき1つのPostgresを使用していてデータが競合するので直列実行にする
+        singleFork: true,
+      },
+    },
     coverage: {
       exclude: [
         "**/generated/**",
