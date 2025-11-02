e1053b282b2a796e76a2f0573e7be8d1ae6456c7
mkizka <g.mkizka@gmail.com>
2025-11-02 17:28:27 +0900

    インデックス作成

--- packages/db/drizzle/0013_create-view-subscriber_followees.sql
+++ packages/db/drizzle/0013_create-view-subscriber_followees.sql
@@ -1,1 +1,3 @@
 CREATE MATERIALIZED VIEW "public"."subscriber_followees" AS (select distinct "follows"."subject_did" from "follows" inner join "subscriptions" on "follows"."actor_did" = "subscriptions"."actor_did");
+--> statement-breakpoint
+CREATE INDEX "subscriber_followees_subject_did_idx" ON "public"."subscriber_followees" USING btree ("subject_did");
