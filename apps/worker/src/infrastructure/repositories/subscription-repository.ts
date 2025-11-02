3975e7cecc5a6e51245f8a42589eaca896839649
mkizka <g.mkizka@gmail.com>
2025-11-02 14:25:13 +0900

    INNER JOINに変えてみる

--- apps/worker/src/infrastructure/repositories/subscription-repository.ts
+++ apps/worker/src/infrastructure/repositories/subscription-repository.ts
@@ -1,6 +1,6 @@
 import type { Subscription, TransactionContext } from "@repo/common/domain";
 import { schema, type SubscriptionInsert } from "@repo/db";
-import { and, eq, exists, inArray } from "drizzle-orm";
+import { eq, inArray } from "drizzle-orm";
 
 import type { ISubscriptionRepository } from "../../application/interfaces/repositories/subscription-repository.js";
 
@@ -66,19 +66,11 @@ export class SubscriptionRepository implements ISubscriptionRepository {
     const result = await ctx.db
       .select({ actorDid: schema.follows.actorDid })
       .from(schema.follows)
-      .where(
-        and(
-          eq(schema.follows.subjectDid, actorDid), // actorDidがフォロイーであるフォロー関係
-          exists(
-            ctx.db
-              .select({ actorDid: schema.subscriptions.actorDid })
-              .from(schema.subscriptions)
-              .where(
-                eq(schema.subscriptions.actorDid, schema.follows.actorDid),
-              ),
-          ),
-        ),
+      .innerJoin(
+        schema.subscriptions,
+        eq(schema.follows.actorDid, schema.subscriptions.actorDid),
       )
+      .where(eq(schema.follows.subjectDid, actorDid))
       .limit(1);
 
     return result.length > 0;
@@ -95,19 +87,11 @@ export class SubscriptionRepository implements ISubscriptionRepository {
     const result = await ctx.db
       .select({ actorDid: schema.follows.actorDid })
       .from(schema.follows)
-      .where(
-        and(
-          inArray(schema.follows.subjectDid, actorDids), // actorDidsのいずれかがフォロイーであるフォロー関係
-          exists(
-            ctx.db
-              .select({ actorDid: schema.subscriptions.actorDid })
-              .from(schema.subscriptions)
-              .where(
-                eq(schema.subscriptions.actorDid, schema.follows.actorDid),
-              ),
-          ),
-        ),
+      .innerJoin(
+        schema.subscriptions,
+        eq(schema.follows.actorDid, schema.subscriptions.actorDid),
       )
+      .where(inArray(schema.follows.subjectDid, actorDids))
       .limit(1);
 
     return result.length > 0;
