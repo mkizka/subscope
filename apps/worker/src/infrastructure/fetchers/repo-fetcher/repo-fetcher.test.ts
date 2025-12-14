4edeb9629b094e4e8c65cac38f58a17a6c3702c3
mkizka <g.mkizka@gmail.com>
2025-12-15 00:43:18 +0900

    testInjectorに置き換え

--- apps/worker/src/infrastructure/fetchers/repo-fetcher/repo-fetcher.test.ts
+++ apps/worker/src/infrastructure/fetchers/repo-fetcher/repo-fetcher.test.ts
@@ -2,12 +2,10 @@ import { readFileSync } from "node:fs";
 import path from "node:path";
 
 import type { AtpBaseClient } from "@repo/client/api";
-import { type IDidResolver } from "@repo/common/domain";
-import { createInjector } from "typed-inject";
 import { describe, expect, test, vi } from "vitest";
-import { mock, mockDeep } from "vitest-mock-extended";
+import { mockDeep } from "vitest-mock-extended";
 
-import type { JobLogger } from "../../../shared/job.js";
+import { testInjector } from "../../../shared/test-utils.js";
 import { RepoFetcher } from "./repo-fetcher.js";
 
 const mockClient = mockDeep<AtpBaseClient>();
@@ -20,23 +18,20 @@ vi.mock("@repo/client/api", () => {
   };
 });
 
-const mockDidResolver = mock<IDidResolver>();
-const mockJobLogger = mock<JobLogger>();
-
-const repoFetcher = createInjector()
-  .provideValue("didResolver", mockDidResolver)
-  .injectClass(RepoFetcher);
-
-const repoCar = readFileSync(
-  path.join(import.meta.dirname, "fixtures", "repo.car"),
-);
-
 describe("RepoFetcher", () => {
+  const didResolver = testInjector.resolve("didResolver");
+  const jobLogger = testInjector.resolve("jobLogger");
+  const repoFetcher = testInjector.injectClass(RepoFetcher);
+
+  const repoCar = readFileSync(
+    path.join(import.meta.dirname, "fixtures", "repo.car"),
+  );
+
   test("正常にリポジトリを取得出来た場合、レコードの配列に変換して返す", async () => {
     // arrange
     // 実際にverifyRepoCarを動作させるために sub.mkizka.dev の実際の情報を使用する
     const did = "did:plc:rt7d3ysf57rqchpqmjk3brc3";
-    mockDidResolver.resolve.mockResolvedValue({
+    didResolver.setResolveResult(did, {
       pds: new URL("https://pds.mkizka.dev"),
       signingKey: "did:key:zQ3shmkscHAeywKVwjvJqW8goQJnwAPL7HnXwZzQuNmPvuy1D",
       handle: "sub.mkizka.dev",
@@ -48,10 +43,9 @@ describe("RepoFetcher", () => {
     });
 
     // act
-    const records = await repoFetcher.fetch(did, mockJobLogger);
+    const records = await repoFetcher.fetch(did, jobLogger);
 
     // assert
     expect(records.length).toBeGreaterThan(0);
-    expect(mockDidResolver.resolve).toHaveBeenCalledWith(did);
   });
 });
