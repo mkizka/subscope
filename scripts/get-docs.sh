fe2e5bbc7651527e3da9c6a79bf7c16c4c6c154a
mkizka <g.mkizka@gmail.com>
2025-12-14 17:40:08 +0900

    get-docs更新

--- scripts/get-docs.sh
+++ scripts/get-docs.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-ATPROTO_SHA=$(< dev-env/.atproto-sha)
+ATPROTO_SHA=$(< packages/client/.atproto-sha)
 
-pnpm repomix --remote https://github.com/bluesky-social/atproto --output docs/repomix/atproto-repomix-output.xml --remote-branch $ATPROTO_SHA
+pnpm repomix --remote https://github.com/bluesky-social/atproto --output docs/repomix/atproto-repomix-output.xml
+pnpm repomix --remote https://github.com/bluesky-social/indigo --output docs/repomix/indigo-repomix-output.xml
 pnpm repomix --remote https://github.com/factory-js/factory-js --output docs/repomix/factoryjs-repomix-output.xml
+pnpm repomix --remote https://github.com/skyware-js/jetstream --output docs/repomix/jetstream-repomix-output.xml
