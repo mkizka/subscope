75d2b7a80048c53323f96ce99e16a49aa7e56039
mkizka <g.mkizka@gmail.com>
2025-12-14 17:11:06 +0900

    Revert "feat(worker): add index-target interfaces and Redis migration task"
    This reverts commit 1b5170063146240a8ceb80663a327a0aaa3906b3.

--- apps/worker/src/domain/index-target/index-target-query.ts
+++
@@ -1,8 +0,0 @@
-import type { Did } from "@atproto/did";
-
-export interface IIndexTargetQuery {
-  isSubscriber: (did: Did) => Promise<boolean>;
-  hasSubscriber: (dids: Did[]) => Promise<boolean>;
-  isTrackedActor: (did: Did) => Promise<boolean>;
-  hasTrackedActor: (dids: Did[]) => Promise<boolean>;
-}
