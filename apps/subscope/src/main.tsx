b22affa51ce28020147a1dce4b1091929ad72133
mkizka <g.mkizka@gmail.com>
2026-01-03 00:06:49 +0900

    prettier/eslint

--- apps/subscope/src/main.tsx
+++ apps/subscope/src/main.tsx
@@ -1,4 +1,7 @@
 import { render } from "preact";
+
 import { App } from "./app.tsx";
 
-render(<App />, document.getElementById("app")!);
+// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+const app = document.getElementById("app")!;
+render(<App />, app);
