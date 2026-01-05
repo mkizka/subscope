654436024d01fd414dece9526162c233661a8215
mkizka <g.mkizka@gmail.com>
2026-01-06 01:42:36 +0900

    設定修正

--- dev-env/scripts/run-dev-env.sh
+++ dev-env/scripts/run-dev-env.sh
@@ -5,4 +5,4 @@ ATPROTO_SHA=$(< ../packages/client/.atproto-sha)
 ATPROTO_DIR="$HOME/.cache/atproto/$ATPROTO_SHA"
 
 echo "[dev-env] Starting atproto dev server..."
-cd $ATPROTO_DIR && make run-dev-env-logged
+cd $ATPROTO_DIR && make run-dev-env
