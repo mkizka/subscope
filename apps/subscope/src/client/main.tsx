c4f2355aed0aa07259b056ac8be164962ea9e991
mkizka <g.mkizka@gmail.com>
2026-01-11 22:21:57 +0900

    add __TANSTACK_QUERY_CLIENT__

--- apps/subscope/src/client/main.tsx
+++ apps/subscope/src/client/main.tsx
@@ -3,6 +3,12 @@ import "./global.css";
 import { render } from "preact";
 
 import { App } from "./app.tsx";
+import { queryClient } from "./lib/trpc.ts";
+
+if (process.env.NODE_ENV === "development") {
+  // @ts-expect-error
+  window.__TANSTACK_QUERY_CLIENT__ = queryClient;
+}
 
 // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 const app = document.getElementById("app")!;
