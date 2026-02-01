1c2fb01ec04009373acca935f640cea043e27f9c
mkizka <g.mkizka@gmail.com>
2026-02-01 22:15:38 +0900

    story追加

--- apps/subscope/vite.config.ts
+++ apps/subscope/vite.config.ts
@@ -3,6 +3,9 @@ import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
+// https://v2.remix.run/docs/guides/vite
+const isStorybook = process.argv[1]?.includes("storybook");
+
 export default defineConfig({
-  plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
+  plugins: [tailwindcss(), !isStorybook && reactRouter(), tsconfigPaths()],
 });
