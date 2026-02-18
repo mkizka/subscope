cd3aab13b93623dea2bad620faa122c8f658ab91
mkizka <g.mkizka@gmail.com>
2026-02-19 01:02:04 +0900

    不要になった処理を削除

--- apps/subscope/server/bootstrap/subscope.ts
+++ apps/subscope/server/bootstrap/subscope.ts
@@ -25,16 +25,6 @@ export class SubscopeServer {
     app.use(loggingMiddleware(logger));
     app.use(express.urlencoded({ extended: true }));
 
-    // 開発環境でのOAuthログイン時 http://127.0.0.1/oauth/callback にリダイレクトされるので、
-    // そこからさらにPUBLIC_URLにリダイレクトさせる
-    app.use((req, res, next) => {
-      if (env.NODE_ENV === "development" && req.hostname === "127.0.0.1") {
-        res.redirect(new URL(req.originalUrl, env.PUBLIC_URL).toString());
-      } else {
-        next();
-      }
-    });
-
     app.use("/oauth", oauthRouter);
     app.use("/images", blobProxyRouter);
     app.use(xrpcRouter);
